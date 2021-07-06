3cbe4dd45c58e4c9ebcf1969975a58d250367fd9
Владимир Капитонов <31070169+VovanchikVP@users.noreply.github.com>
2021-07-07 09:20:50 +1000

    Update func.py
    Добавлены комментарии

--- schedule/func.py
+++ schedule/func.py
@@ -65,28 +65,51 @@ class GetDateFile:
         self.add_data_db()
 
     def _root_element(self):
+        """
+        Получение названия корневого тега
+        """
         r = re.compile(r'<([^(?xml)]\w+\b)', re.I | re.M)
         s = r.search(self.file_data)
         self.root_element = s.group(1)
 
     def _data_element(self):
+        """
+        Парсинг заданных параметров.
+        """
+        # Содержимое тега data
         r_data = re.compile(r'<data.*?>(.*)</data>', re.I | re.M | re.S)
         s_data = r_data.search(self.file_data).group(1)
+        
+        # Данные из тега purchaseNumber вложенного в тег data
         r_purchaseNumber = re.compile(r'<.*?:purchaseNumber>(.*?)</.*?:purchaseNumber>', re.I | re.M | re.S)
         self.purchaseNumber = r_purchaseNumber.search(s_data).group(1)
+        
+        # Данные из тега docPublishDate вложенного в тег data
         r_docPublishDate = re.compile(r'<.*?:docPublishDate>(.*?)</.*?:docPublishDate>', re.I | re.M | re.S)
         self.docPublishDate = r_docPublishDate.search(s_data).group(1)
         self._convert_date()
+        
+        # Данные из тега purchaseObjectInfo вложенного в тег data
         r_purchaseObjectInfo = re.compile(r'<.*?:purchaseObjectInfo>(.*?)</.*?:purchaseObjectInfo>', re.I | re.M | re.S)
         self.purchaseObjectInfo = r_purchaseObjectInfo.search(s_data).group(1)
+        
+        # Данные из тега responsibleOrg вложенного в тег data
         r_responsibleOrg = re.compile(r'<.*?responsibleOrg>(.*)</.*?responsibleOrg>', re.I | re.M | re.S)
         s_responsibleOrg = r_responsibleOrg.search(s_data).group(1)
+        
+        # Данные из тега regNum вложенного в тег data/responsibleOrg
         r_responsibleOrg_regNum = re.compile(r'<.*?:regNum>(.*?)</.*?:regNum>', re.I | re.M | re.S)
         self.responsibleOrg_regNum = r_responsibleOrg_regNum.search(s_responsibleOrg).group(1)
+        
+        # Данные из тега fullName вложенного в тег data/responsibleOrg
         r_responsibleOrg_fullName = re.compile(r'<.*?:fullName>(.*?)</.*?:fullName>', re.I | re.M | re.S)
         self.responsibleOrg_fullName = r_responsibleOrg_fullName.search(s_responsibleOrg).group(1)
+        
+        # Данные из тега lot вложенного в тег data
         r_lot = re.compile(r'<.*?:lot>(.*)</.*?:lot>', re.I | re.M | re.S)
         s_lot = r_lot.search(s_data).group(1)
+        
+        # Данные из тега maxPrice вложенного в тег data/lot
         r_lot_maxPrice = re.compile(r'<.*?:maxPrice>(.*?)</.*?:maxPrice>', re.I | re.M | re.S)
         self.lot_maxPrice = r_lot_maxPrice.search(s_lot).group(1)
 
@@ -104,6 +127,9 @@ class GetDateFile:
         procedure.save()
 
     def _convert_date(self):
+        """
+        Формирует строку с датой для сохранения в базе данных из данных указанных в теге docPublishDate.
+        """
         r = re.compile(r'^([0-9]{4}-[0-1][0-9]-[0-3][0-9])')
         s = r.search(self.docPublishDate)
         self.docPublishDate = s.group(1)
